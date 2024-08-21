db15f565182cba16a9f4d617ad024715a44c4ed6
VladWhite22 <vladislav22971@mail.ru>
2024-08-22 00:28:07 +0300

    KS-2-1 Среднее арифметическое

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val student1: Float = 3f
+    val student2: Float = 4f
+    val student3: Float = 3f
+    val studend4: Float = 5f
+    val midValue: Float = (student1 + student2 + student3 + studend4) / 4
+    println(midValue)
+}
