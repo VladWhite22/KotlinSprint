1fa3ab6693e3c04d3649703a55387911b3e2ffc5
VladWhite22 <vladislav22971@mail.ru>
2024-08-21 16:31:21 +0300

    KS-1-2 Имитированы логи с сервера

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thankText: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(thankText)
+
+    //val numberOfEmployers = 2000
+    val numberOfEmployers = 1999
+    println(numberOfEmployers)
+}
