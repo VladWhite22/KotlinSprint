ac1fe59e1fecc2e03228326768c2ffe2ca685ab9
VladWhite22 <vladislav22971@mail.ru>
2024-08-21 17:22:34 +0300

    KS-1-5 Время проведенное в космосе

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_1
+
+fun main() {
+    val seconds: Int = 6480
+    val fullHour: String = "01"
+    val fullMinutes: String = "48"
+    val fullSeconds: String = "00"
+    println("$fullHour:$fullMinutes:$fullSeconds")
+}
