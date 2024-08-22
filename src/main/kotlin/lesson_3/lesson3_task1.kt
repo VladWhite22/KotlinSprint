35385843d5b3aa12326c47c49e2730bdf848325f
VladWhite22 <vladislav22971@mail.ru>
2024-08-23 02:15:47 +0300

    KS-3-1 Приветствие в проложении

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_3
+
+fun main() {
+    val greetings: String = "Добрый"
+    val name: String = "Михаил"
+
+    println("$greetings день $name!")
+    println("$greetings вечер $name!")
+}
