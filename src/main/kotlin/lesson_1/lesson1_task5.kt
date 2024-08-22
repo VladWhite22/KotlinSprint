97c4d2d86c63da8d2ce2d60fdaca37d674527fc7
VladWhite22 <vladislav22971@mail.ru>
2024-08-22 12:49:51 +0300

    KS-1-5 Время проведенное в космосе Исправленный

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,8 +2,9 @@ package org.example.lesson_1
 
 fun main() {
     val seconds: Int = 6480
-    val fullHour: String = "01"
-    val fullMinutes: String = "48"
-    val fullSeconds: String = "00"
+    val fullHour: String = String.format("%02d", seconds / 3600)
+    val fullMinutes: String = String.format("%02d", (seconds % 3600) / 60)
+    val fullSeconds: String = String.format("%02d", (seconds % 60))
+
     println("$fullHour:$fullMinutes:$fullSeconds")
 }
