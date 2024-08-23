739d7470094786c17943d5819f60ba1e5273e0eb
VladWhite22 <vladislav22971@mail.ru>
2024-08-24 00:15:22 +0300

    KS-3-2 Приложение персональных данных Исправленный

--- src/main/kotlin/lesson_3/lesson3_task2.kt
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -1,15 +1,15 @@
 package org.example.lesson_3
 
 fun main() {
-    val name = "Татьяна"
-    val surname = "Сергеевна"
+    var name = "Татьяна"
+    var surname = "Сергеевна"
     var lastname = "Андреева"
     var age = 20
     println("$name $surname $lastname, $age")
 
-    val name1 = "Татьяна"
-    var surname1 = "Сергеевна"
-    var lastname1 = "Сидорова"
-    var age1 = 22
-    println("$name1 $surname1 $lastname1, $age1")
+     name = "Татьяна"
+     surname = "Сергеевна"
+     lastname = "Сидорова"
+     age = 22
+    println("$name $surname $lastname, $age")
 }
