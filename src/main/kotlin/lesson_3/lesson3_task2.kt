49ff375d8806cd45544d176932483e08eaae2059
VladWhite22 <vladislav22971@mail.ru>
2024-08-23 22:22:05 +0300

    KS-3-2 Приложение  персональных данных

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val name = "Татьяна"
+    val surname = "Сергеевна"
+    var lastname = "Андреева"
+    var age = 20
+    println("$name $surname $lastname, $age")
+
+    val name1 = "Татьяна"
+    var surname1 = "Сергеевна"
+    var lastname1 = "Сидорова"
+    var age1 = 22
+    println("$name1 $surname1 $lastname1, $age1")
+}
