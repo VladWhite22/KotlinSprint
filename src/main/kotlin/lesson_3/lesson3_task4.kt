ab38a5592529482e0dceaa7cfbeff07342d64e92
VladWhite22 <vladislav22971@mail.ru>
2024-08-24 00:21:54 +0300

    KS-3-4 Приложение шахматы

---
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    var moveFrom = "E2"
+    var moveIn = "E4"
+    var moveNumber = 1
+
+    println("[$moveFrom-$moveIn;$moveNumber]")
+
+    moveFrom = "D2"
+    moveIn = "D3"
+    moveNumber = 2
+
+    println("[$moveFrom-$moveIn;$moveNumber]")
+}
