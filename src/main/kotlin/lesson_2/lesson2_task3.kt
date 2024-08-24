6fe8e3086cea68771218d832ff52625806ce4df2
VladWhite22 <vladislav22971@mail.ru>
2024-08-24 18:23:26 +0300

    KS-2-3 Программа рассчитывающая время прибытия Исправленный

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,17 @@
 package org.example.lesson_2
 
+const val MINUTES_IN_HOUR = 60
+
 fun main() {
     val startTimeHour = 9
     val startTimeMinute = 39
     val travelTimeMinute = 457
-    val minutesInHour = 60
-    val sumStartTimeMinute = (startTimeHour * minutesInHour) + startTimeMinute
 
-    val finishHour = (sumStartTimeMinute + travelTimeMinute) / minutesInHour
-    val finishMinute = (sumStartTimeMinute + travelTimeMinute) % minutesInHour
+    val sumStartTimeMinute = (startTimeHour * MINUTES_IN_HOUR) + startTimeMinute
+
+    val finishHour = (sumStartTimeMinute + travelTimeMinute) / MINUTES_IN_HOUR
+    val finishMinute = (sumStartTimeMinute + travelTimeMinute) % MINUTES_IN_HOUR
 
     println(String.format("%02d:%02d", finishHour, finishMinute))
 }
+
