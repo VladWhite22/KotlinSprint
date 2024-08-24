a21ff02c42710d707bad0667e090c3b8605a2c11
VladWhite22 <vladislav22971@mail.ru>
2024-08-24 18:28:41 +0300

    KS-2-5 Программа калькулятор для банка Исправленный2

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,12 +2,13 @@ package org.example.lesson_2
 
 import kotlin.math.pow
 
+const val PERCENT = 100
 fun main() {
     val depositedMoney: Int = 70000
     val interestRate: Double = 16.7
     val depositedTime: Double = 20.0
-    val percent = 100
 
-    val totalMoney = depositedMoney * (((percent + interestRate) / percent).pow(depositedTime))
+
+    val totalMoney = depositedMoney * (((PERCENT + interestRate) / PERCENT).pow(depositedTime))
     println(String.format("%.3f", totalMoney))
 }
