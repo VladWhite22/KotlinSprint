785cac1a555d5be3b61f2b1cf95e9223c6918b6e
VladWhite22 <vladislav22971@mail.ru>
2024-08-28 20:50:18 +0300

    KS-5-2 Проверка возраста Исправленный

--- src/main/kotlin/lesson_5/lesson5_task2.kt
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -4,7 +4,12 @@ const val AGE = 18
 
 fun main() {
     val currentYear = 2024
-    val birthYear = 1997
-    if (currentYear - birthYear > AGE) println("Показать экран со скрытым контентом")
-    else println("Доступ ограничен")
+
+    println("Введите год рождения:")
+    val birthYear = readln().toInt()
+
+    if (currentYear - birthYear > AGE)
+        println("Показать экран со скрытым контентом")
+    else
+        println("Доступ ограничен")
 }
