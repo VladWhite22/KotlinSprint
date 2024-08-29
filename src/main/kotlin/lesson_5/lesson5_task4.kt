64c5606c9518d314310d8caad8fe4ec121b95b72
VladWhite22 <vladislav22971@mail.ru>
2024-08-29 22:31:55 +0300

    KS-5-4 Верификация на космическом корабле Исправленный1

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_5
 
-fun main(){
-    val nameBookd ="Zaphod"
-    val passwordBookd ="PanGalactic"
+fun main() {
+    val nameBookd = "Zaphod"
+    val passwordBookd = "PanGalactic"
 
-    println("Приветствуем вас на корабле Heart of Gold! \n" +
-             "Введите ваше имя:")
-    val name = readln().toString()
-    if(name==nameBookd) println("Введите пароль:")
-    val password = readln().toString()
-    if (name != nameBookd) println("Зарегестрируйте нового пользователя:")
-    val nameNewUser= readln().toString()
-    val passwordNewUser= readln().toString()
-    when{password==passwordBookd-> println("Вы можете можете управлять кораблем и осуществлять навигацию по вселенной!") }
+    println(
+        "Приветствуем вас на корабле Heart of Gold! \n" +
+                "Введите ваше имя:"
+    )
+    val name = readln()
+    if (name == nameBookd) println("Введите пароль:")
+    val password = readln()
+    if (name != nameBookd) println("Зарегестрируйте нового пользователя!")
+    if (password != passwordBookd) println("Зарегестрируйте нового пользователя!")
+    when {password == passwordBookd -> println("Вы можете можете управлять кораблем и осуществлять навигацию по вселенной!") }
 }
