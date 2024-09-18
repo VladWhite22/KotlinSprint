8c1a19928c38a3a9b2bde366ae229157b9255608
VladWhite22 <156139906+VladWhite22@users.noreply.github.com>
2024-09-18 23:28:44 +0300

    Delete src/main/kotlin/lesson_7/lesson7_task3.kt

--- src/main/kotlin/lesson_7/lesson7_task3.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_7
-
-fun main() {
-    println("Введите число:")
-    val userNumber = readln().toInt()
-    val progression = 0..userNumber step 2
-    for (n in progression) println(n)
-}
