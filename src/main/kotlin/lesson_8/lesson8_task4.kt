8e4a3b99cef483c3ae96808208120be08f19df9f
VladWhite22 <vladislav22971@mail.ru>
2024-09-24 23:27:47 +0300

    KS-8-4 Замена ингридиента в рецепте Исправленный

--- src/main/kotlin/lesson_8/lesson8_task4.kt
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -2,19 +2,15 @@ package org.example.lesson_8
 
 fun main() {
     var pelmeni = arrayOf("фарш", "перец молотый", "соль", "тесто")
-    println("${pelmeni.joinToString()} \nВведите ингредиент который хотите заменить:")
-    val userIngredient = readln()
-    var caunter = 0
 
-    for (i in pelmeni) {
-        caunter++
-        if (i == userIngredient) {
-            println("Введите ингредиент который хотите добавить:")
-            val userIngredientNew = readln()
-            pelmeni[caunter - 1] = userIngredientNew
-            println("Готово! Вы сохранили следующий список:\n${pelmeni.joinToString()}")
-            break
-        }
-        if (caunter == pelmeni.size && i != userIngredient) println("Ингредиента в рецепте нет!")
-    }
+    println("${pelmeni.joinToString()} \nВведите ингредиент который хотите заменить:")
+
+    val userIngredient = readln()
+
+    if (userIngredient in pelmeni) {
+        println("Введите ингредиент который хотите добавить:")
+        val userIngredientNew = readln()
+        pelmeni[pelmeni.indexOf(userIngredient)] = userIngredientNew
+        println("Готово! Вы сохранили следующий список:\n${pelmeni.joinToString()}") }
+    else println("Ингредиента в рецепте нет!")
 }
