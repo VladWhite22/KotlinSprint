3e2034f0cef3f42144e40d980aed6efdfa62cfec
VladWhite22 <vladislav22971@mail.ru>
2024-09-25 19:32:25 +0300

    KS-8-5 Массив задаваеваемый пользователем из консоли

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_8
+
+fun main() {
+
+    println("Введите кол-во элементов:")
+    val numberOfElements = readln().toInt()
+    var array = Array(numberOfElements){""}
+    for (i in array.indices) {
+        println("Введите элемент массива:")
+        val newElement = readln()
+        array[i] += newElement
+    }
+    println(array.joinToString())
+}
