5708e047b420d11fa0d078eb66fd78eb4714cc5e
VladWhite22 <vladislav22971@mail.ru>
2024-10-22 02:03:21 +0300

    KS-11-4 Сущности для дизайн проекта

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_11
+
+fun main() {
+
+}
+class Сategory(
+    val categoryName: String,
+    val categoryDescription: String,
+)
+
+class Dish(
+    val dishName: String,
+    val favorit: Boolean,
+)
+
+class Ingredient(
+    val ingredient: String,
+    val qantity: Int,
+    val portion: Int,
+)
