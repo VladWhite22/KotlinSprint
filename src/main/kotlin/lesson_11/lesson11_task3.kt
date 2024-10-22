92a672b808b1d5ce56d5300bfadb9381c5e6fe7c
VladWhite22 <vladislav22971@mail.ru>
2024-10-22 22:42:59 +0300

    KS-11-Проектирование сущностей для социальной сети 1

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -1,38 +1,31 @@
 package org.example.lesson_11
 
 fun main() {
-    val user1 = Room.User("(ಠ_ಠノ)", "Alex")
-    val user2 = Room.User("（≧ロ≦）", "Oleg")
+    val user1 = User("(ಠ_ಠノ)", "Alex", "Разговаривает")
+    val user2 = User("（≧ロ≦）", "Oleg", "Микрофон выключен")
 
-    val room1 = Room("Kotlin","Class")
+    val room1 = Room("Kotlin", "1")
 
-    user1.updateStatus()
     room1.addUser(user1)
     room1.addUser(user2)
 }
-
+class User(
+    var avatar: String,
+    var nickname: String,
+    var userStatus: String,
+)
 class Room(
     val cover: String,
     val title: String,
-
-) {
-    var usersList: MutableList<User>? = null
-
-    class User(
-       var avatar: String,
-       var nickname: String,
-        ) {
-
-        var userStatus: Array<String> = arrayOf("разговаривает", "микрофон выключен", "пользователь заглушен")
-        fun updateStatus() {
-            println("Введите новый nickname:")
-            nickname= readln()
-            val newStatus = userStatus.random()
-            println("$nickname $newStatus")
-        }
-    }
-
+    var usersList: ArrayList<User> = ArrayList<User>(),)
+{
     fun addUser(user: User) {
-        usersList?.add(user)
+        usersList.add(user)
+    }
+    fun updateStatus(nickname: User, userStatus: String) {
+        println("Введите новый nickname:")
+        nickname = readln()
+        userStatus = readln()
+        println("$nickname $userStatus")
     }
 }
