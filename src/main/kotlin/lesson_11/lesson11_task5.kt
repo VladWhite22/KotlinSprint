cc7b0bcaea418f3e931149e3004007da1aee2ca2
VladWhite22 <vladislav22971@mail.ru>
2024-10-28 21:41:29 +0300

    KS-11-5 Реализовать класс работы форума Исправленный4

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,24 +1,25 @@
 package org.example.lesson_11
 
-fun main(){
-
-
+fun main() {
     val forum1 = Forum()
     forum1.createNewUser("Gor")
     forum1.createNewUser("gfgf")
-    forum1.createNewMessage(1,"fdfdf")
+    forum1.createNewMessage(1, "fdfdf")
 }
+
 class ForumMember(
     var userId: Int,
-    val userName: String,)
+    val userName: String,
+)
 
 class ForumMesage(
     var authoId: Int,
-    var message: String,)
+    var message: String,
+)
 
-class Forum (){
-    val forumMembers:MutableList<ForumMember> = mutableListOf()
-    val forumMesage:MutableList<ForumMesage> = mutableListOf()
+class Forum {
+    val forumMembers: MutableList<ForumMember> = mutableListOf()
+    val forumMesage: MutableList<ForumMesage> = mutableListOf()
 
     fun createNewUser(userName: String) {
         var id = forumMembers.size
@@ -28,20 +29,13 @@ class Forum (){
 
     fun createNewMessage(authoId: Int, message: String) {
         val _forumMesage = ForumMesage(authoId = authoId, message = message)
-        for (i in forumMembers.userId){
-            if( i == _forumMesage.authoId)
-            forumMesage.add(_forumMesage)
-        }
-        //    fun createNewMessage(authoId: Int,message: String) {
-//        val _forumMesage = ForumMesage(authoId = authoId, message = message)
-//        var a = 0
-//        for (i in 0..999){
-//            if( forumMembers[i].userId == _forumMesage.authoId)
-//                forumMesage.add(_forumMesage)
-//            a++
-//        }
+        forumMembers.find { it.userId == authoId }
+        forumMesage.add(_forumMesage)
     }
+
     fun printThread() {
-        println("${forumMembers.authoId}:${forumMesage.message}")
+        for (a in forumMembers.userId)
+        for (b in forumMembers.userName)
+            println("$a:$b")
     }
 }
