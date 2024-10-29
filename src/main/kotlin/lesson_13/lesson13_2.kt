2c5095f12cea3cdf2b7cfd90c4900f000cf6ed9b
VladWhite22 <vladislav22971@mail.ru>
2024-10-30 01:16:38 +0300

    KS-13-2 Класс с nullable типами и выводом в консоль Исправленный

--- src/main/kotlin/lesson_13/lesson13_2.kt
+++ src/main/kotlin/lesson_13/lesson13_2.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_13
 
 fun main() {
-    val company1 = Company("Олег", 89606457845, null)
+    val company1 = Company("Олег", 89606457845)
     company1.printCompany()
 }
 
 class Company(
     var name: String?,
     var phoneNumber: Long?,
-    var company: String?, ) {
+    var company:String? = null, ) {
 
     fun printCompany(){
         when {company == null -> company = "<не указано>" }
