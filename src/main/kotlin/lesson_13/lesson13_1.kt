98c355f4635a99b9fc0153931a8edcb8f41358a6
VladWhite22 <vladislav22971@mail.ru>
2024-10-29 22:29:09 +0300

    KS-13-1 Класс с nullable типами

---
+++ src/main/kotlin/lesson_13/lesson13_1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_13
+
+fun main() {
+    val company1 = CompanyInformation(null, null, null)
+}
+
+class CompanyInformation(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+)
