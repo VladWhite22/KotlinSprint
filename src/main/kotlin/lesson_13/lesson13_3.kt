6ec82296ce1de3147d2852bcc9ee34f1845c221d
VladWhite22 <vladislav22971@mail.ru>
2024-10-31 02:22:24 +0300

    KS-13-3  Вывод компаний из телефонной книги Исправленный 1

--- src/main/kotlin/lesson_13/lesson13_3.kt
+++ src/main/kotlin/lesson_13/lesson13_3.kt
@@ -1,19 +1,19 @@
 package org.example.lesson_13
 
 fun main() {
-    val contact1 = Companys("Олег", 89606457845, null)
-    val contact2 = Companys("Игорь", 89005553535, null)
-    val contact3 = Companys("Анна", 89034434354, "FNX")
-    val contact4 = Companys(null, 83443434334, null)
-    val contact5 = Companys("Эдуард", 89232344323, "AML")
+    val contact1 = Company1("Олег", 89606457845, null)
+    val contact2 = Company1("Игорь", 89005553535, null)
+    val contact3 = Company1("Анна", 89034434354, "FNX")
+    val contact4 = Company1("Ольга", 83443434334, null)
+    val contact5 = Company1("Эдуард", 89232344323, "AML")
 
-    val listContacts = listOf<Companys>(contact1, contact2, contact3, contact4, contact5)
+    val listContacts = listOf<Company1>(contact1, contact2, contact3, contact4, contact5)
 
-    for (company in listContacts)
-        if (company.company != null) println(company.company)
+    listContacts.map { if (it.company != null) println(it.company) }
 }
 
-class Companys(
-    var name: String?,
-    var phoneNumber: Long?,
-    var company: String?,)
+class Company1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?,
+)
