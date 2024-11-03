b6537664ab358e9fa34962ada7e9abb26172daaf
VladWhite22 <vladislav22971@mail.ru>
2024-11-04 02:43:01 +0300

    KS-13-4 Программа для добавления контакта в телефонную книгу Исправленный

--- src/main/kotlin/lesson_13/lesson13_4.kt
+++ src/main/kotlin/lesson_13/lesson13_4.kt
@@ -2,31 +2,20 @@ package org.example.lesson_13
 
 fun main() {
     val contact = Company2()
-    contact.addContact()
-    contact.addContact()
-    contact.printContacts()
-}
-
-class Company2 {
-    var listContacts = mutableListOf<Company2>()
-    val name: String = ""
-    val phoneNumber: Long? = null
-    val company: String? = null
 
     fun addContact(
         _name: String = readln(),
         _phoneNumber: Long? = readln().toLongOrNull(),
         _company: String? = readln()
-
     ) {
         if (_phoneNumber == null) {
             return println("Контакт не добавлен! Вы не ввели номер телефона.")
         }
-        listContacts.add(Company2())
+        contact.listContacts.add(Company2(_name, _phoneNumber, _company))
     }
 
     fun printContacts() {
-        for (i in listContacts) {
+        for (i in contact.listContacts) {
             println(
                 "${i.name}" +
                         "\n${i.phoneNumber}" +
@@ -34,4 +23,17 @@ class Company2 {
             )
         }
     }
+    addContact()
+    addContact()
+    printContacts()
 }
+
+
+class Company2(
+    val name: String? = null,
+    val phoneNumber: Long? = null,
+    val company: String? = null
+) { val listContacts = mutableListOf<Company2>() }
+
+
+
