91f50923cff33ef9ca287e88d1b3715434abdc28
VladWhite22 <vladislav22971@mail.ru>
2024-11-07 17:27:51 +0300

    KS-15-2 Мониторинг погодных условий Исправленный

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -4,27 +4,22 @@ fun main() {
     val temperature1 = Temperature(1, "Лето", 23, 19)
     val precipitationAmount1 = PrecipitationAmount(1, "Лето", 10, 0)
 
-    WeatherServer().SendMassege(temperature1, precipitationAmount1)
+    WeatherServer().SendMassege(temperature1)
 }
 
 class WeatherServer {
-    fun SendMassege(temperature: Temperature, precipitationAmount: PrecipitationAmount) {
-        var weatherList1 = mutableListOf<Temperature>()
-        var weatherList2 = mutableListOf<PrecipitationAmount>()
-        weatherList1.add(temperature)
-        weatherList2.add(precipitationAmount)
+    fun SendMassege(parametr: WeatherStationStats) {
+        var weatherList = mutableListOf<WeatherStationStats>()
 
-        for (weater in weatherList1) {
+        weatherList.add(parametr)
+
+        for (weater in weatherList) {
             println("${weater.dayNumber} ${weater.season}" +
                     "\n${weater.dayTemperature} ${weater.nightTemperature}") }
-
-        for (weater in weatherList2) {
-            println("${weater.dayNumber} ${weater.season}" +
-                    "\n${weater.dayAmount} ${weater.nightAmout}") }
     }
 }
 
-abstract class WeatherStationStats {
+abstract class WeatherStationStats() {
     abstract val dayNumber: Int
     abstract val season: String
 }
