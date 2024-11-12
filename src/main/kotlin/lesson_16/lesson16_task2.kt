a4dfd0a60f55fbab7644b34b6794ee6711d4c060
VladWhite22 <vladislav22971@mail.ru>
2024-11-12 15:53:49 +0300

    KS-16-2 Длинна окружности и площадь с привытными данными

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -2,18 +2,21 @@ package org.example.lesson_16
 
 import kotlin.math.pow
 
-fun main(){
-val circle1= Circle(7)
-    println("${circle1.calculateLength()}" +
-            "\n${circle1.calculateArea()}")
+fun main() {
+    val circle1 = Circle(7)
+    println(
+        "${circle1.calculateLength()}" +
+                "\n${circle1.calculateArea()}"
+    )
 }
 
-class Circle(private val radius:Int){
-   private val pi = 3.14
-    fun calculateLength():Double{
-       return 2*pi*radius
+class Circle(private val radius: Int) {
+    private val pi = 3.14
+    fun calculateLength(): Double {
+        return 2 * pi * radius
     }
-    fun calculateArea():Double{
-        return (pi*radius).pow(2)
+
+    fun calculateArea(): Double {
+        return (pi * radius).pow(2)
     }
 }
