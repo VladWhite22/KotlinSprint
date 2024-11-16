1d1dbbfbdf48fa7bb5a76154789fce65d73adb37
VladWhite22 <vladislav22971@mail.ru>
2024-11-16 03:32:33 +0300

    KS-17-4 Отслеживание посылки

--- src/main/kotlin/lessson_17/lesson_17_task4.kt
+++ src/main/kotlin/lessson_17/lesson_17_task4.kt
@@ -1,21 +1,19 @@
 package org.example.lessson_17
 
-fun main(){
-val package1 = Package(1,"На сц")
+fun main() {
+    val package1 = Package(1, "На сц")
     println(package1.counter)
-    package1.location="В пути"
+    package1.location = "В пути"
     println(package1.counter)
-    package1.location="В пути"
+    package1.location = "В пути"
     println(package1.counter)
 }
 
-class Package( _packageNumber: Int, _location: String) {
-    var counter:Int = 0
+class Package(_packageNumber: Int, _location: String) {
+    var counter: Int = 0
     var packageNumber = _packageNumber
     var location = _location
-    set(value) {
-        field = value
-        if (field != value) counter++
-    }
-
+        set(value) {
+            if (field != value) counter++
+        }
 }
