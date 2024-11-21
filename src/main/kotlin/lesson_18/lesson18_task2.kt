7f8bed2fbdbd0ab12b98b6fd6fb3b46946fcbdcd
VladWhite22 <vladislav22971@mail.ru>
2024-11-22 00:07:14 +0300

    KS-18-2 Игра "игральные кости" полиморфизм включения Исправленный3

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -14,26 +14,18 @@ fun main() {
 abstract class Dice {
     abstract val number: Int
     open fun throwDice() {
-        return println((1..number).random())}
+        return println((1..number).random())
+    }
 }
 
 class FourSidedDice() : Dice() {
     override val number: Int = 4
-    override fun throwDice() {
-        super.throwDice()
-    }
 }
 
 class SixSidedDice : Dice() {
     override val number: Int = 6
-    override fun throwDice() {
-        super.throwDice()
-    }
 }
 
 class EightSidedDice : Dice() {
     override val number: Int = 8
-    override fun throwDice() {
-        super.throwDice()
-    }
 }
