1d7690238145c2f37bc08ae00a05416206b8bbea
VladWhite22 <vladislav22971@mail.ru>
2024-11-21 16:38:34 +0300

    KS-19-3 Класс без реаилизации методов TODO

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_19
+
+fun main() {
+    SpaceShip().Land()
+}
+
+class SpaceShip {
+    fun TakeOff() {
+        TODO()
+    }
+
+    fun Land() {
+        TODO("Добавить реализацию метода!")
+    }
+
+    fun ShootAsteroids() {
+        TODO("Not yet implemented")
+    }
+}
