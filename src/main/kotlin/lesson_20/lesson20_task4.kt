84cc12d82b245c72313ea9d5c664e5b74791b622
VladWhite22 <vladislav22971@mail.ru>
2024-11-26 14:46:56 +0300

    KS-20-4 Преобразование списка строк map

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -2,6 +2,5 @@ package org.example.lesson_20
 
 fun main() {
     val stringList = listOf("0", "1", "2", "3", "4")
-
     stringList.mapIndexed { idx, it: String -> if (idx % 2 == 0) println("Нажат элемент $it") }
 }
