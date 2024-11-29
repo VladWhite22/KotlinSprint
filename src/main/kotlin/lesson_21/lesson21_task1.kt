641fa5b07b2b61d9174f443de498487845681c23
VladWhite22 <vladislav22971@mail.ru>
2024-11-29 22:15:13 +0300

    KS-21-1 extension функция для String

--- src/main/kotlin/lesson_21/lesson21_task1.kt
+++ src/main/kotlin/lesson_21/lesson21_task1.kt
@@ -1,17 +1,12 @@
 package org.example.lesson_21
 
 fun main() {
-
     val text = "hello kotlin"
     println(text.vowelCount())
 }
 
 fun String.vowelCount(): Int {
     val listOfGuestLetters = listOf('a', 'e', 'i', 'o', 'u')
-//    var counter = 0
-//
-//        if (char in listOfGuestLetters)
-//            counter++
-    return this.count {it in this.contains(listOfGuestLetters)}
+    return this.count { listOfGuestLetters.contains(it) }
 }
 
