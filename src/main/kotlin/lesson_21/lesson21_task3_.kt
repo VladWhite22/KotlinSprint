882418dca4fccd1e536b00777b83b169e7297be1
VladWhite22 <vladislav22971@mail.ru>
2024-12-01 22:10:46 +0300

    KS-21-3 extension функция  isHealthy для класса Player

---
+++ src/main/kotlin/lesson_21/lesson21_task3_.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_21
+
+
+fun main() {
+    val player1 = Player1("Alex", 100, 100)
+    println(player1.isHealthy())
+}
+
+class Player1(val name: String, val fullHealth: Int, var currentHealth: Int)
+
+fun Player1.isHealthy(): Boolean {
+    return if (this.fullHealth == this.currentHealth) true
+    else false
+}
