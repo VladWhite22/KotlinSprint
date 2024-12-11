acceaf84de02439568a987a057168a3ee1bc4c1a
VladWhite22 <vladislav22971@mail.ru>
2024-12-11 03:05:22 +0300

    KS-21-4 extension функция записывающая слово в файл Исправленный

--- src/main/kotlin/lesson_21/lesson21_task4.kt
+++ src/main/kotlin/lesson_21/lesson21_task4.kt
@@ -1,15 +1,14 @@
 package org.example.lesson_21
+
 import java.io.File
 import java.util.*
 
 fun main() {
     val file1 = File("file1.txt")
-    file1.addText("TEXT2")
+    file1.addText("TEXT1")
 }
 
 fun File.addText(newText: String) {
-    val listText = this.readLines()
-   val newElement = this.appendText(newText.lowercase(Locale.getDefault()))
-   val resut = listText.addFirst(newElement.toString())
-    return resut
+    val content = this.readText()
+    this.writeText(newText.lowercase(Locale.getDefault()) + "\n" + content)
 }
